276e9fe82a19ce3ff20b3ccbd8f4642bfe7b9e4d
mrskisawesome <fkempton38@gmail.com>
2024-01-28 16:10:43 +0000

    form working

--- src/app/submitCocktail/page.js
+++ src/app/submitCocktail/page.js
@@ -1,9 +1,28 @@
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 export default function submitCocktail() {
+  async function handleAddCocktail(formData) {
+    "use server";
+    //get formdata
+    const cocktail_name = formData.get("cocktail_name");
+    const ingredients = formData.get("ingredients");
+    const instructions = formData.get("instructions");
+    const username = formData.get("username");
+
+    //make the sql request
+    await sql`INSERT INTO submissions (cocktail_name, ingredients, instructions, username) VALUES (${cocktail_name},${ingredients},${instructions},${username})`;
+    // revalidate the path so the new item shows
+    revalidatePath("/contributions");
+
+    // take me to the home page
+    redirect("/contributions");
+  }
   return (
     <>
       <div>
         <h2>Submit your Recipe here</h2>
-        <form className="form">
+        <form action={handleAddCocktail}>
           <label htmlFor="cocktail_name">Name of Cocktail</label>
           <input
             name="cocktail_name"
@@ -26,7 +45,8 @@ export default function submitCocktail() {
           />
 
           <label htmlFor="username">Submitted by:</label>
-          <input name="username" id="username" placeholder="username" />
+          <input name="username" id="username" placeholder="Username" />
+
           <button type="submit">Add My Cocktail</button>
         </form>
       </div>
