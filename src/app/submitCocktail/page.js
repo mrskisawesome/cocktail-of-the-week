1bbfba9f18e77a0e1da7ec0fe88faef1f611f3fe
mrskisawesome <fkempton38@gmail.com>
2024-01-28 15:36:10 +0000

    form added

--- src/app/submitCocktail/page.js
+++ src/app/submitCocktail/page.js
@@ -3,6 +3,32 @@ export default function submitCocktail() {
     <>
       <div>
         <h2>Submit your Recipe here</h2>
+        <form className="form">
+          <label htmlFor="cocktail_name">Name of Cocktail</label>
+          <input
+            name="cocktail_name"
+            id="cocktail_name"
+            placeholder="Name of Cocktail"
+          />
+
+          <label htmlFor="ingredients">Ingredients</label>
+          <input
+            name="ingredients"
+            id="ingredients"
+            placeholder="Ingredients"
+          />
+
+          <label htmlFor="instructions">Mixing instructions</label>
+          <input
+            name="instructions"
+            id="instructions"
+            placeholder="Instructions"
+          />
+
+          <label htmlFor="username">Submitted by:</label>
+          <input name="username" id="username" placeholder="username" />
+          <button type="submit">Add My Cocktail</button>
+        </form>
       </div>
     </>
   );
