04be2c8d6eb94526afadd1619ea64f9af608ae4c
mrskisawesome <fkempton38@gmail.com>
2024-01-28 14:12:05 +0000

    vercel postgres linked

--- src/app/contributions/page.js
+++ src/app/contributions/page.js
@@ -2,11 +2,24 @@ export const metadata = {
   title: "Contributions",
   description: "Recipes from readers",
 };
-export default function Contributions() {
+import { sql } from "@vercel/postgres";
+export default async function Contributions() {
+  const posts = await sql`SELECT * FROM submissions`;
   return (
     <div>
       <h2>Your contributions</h2>
-      <p>blah blah blah</p>
+      <div className="submissions">
+        {posts.rows.map((post) => {
+          return (
+            <div key={post.username} className="head">
+              <h3>{post.cocktail_name}</h3>
+              <p>Ingredients: {post.ingredients}</p>
+              <p>Instructions: {post.instructions}</p>
+              <h5>Submitted by {post.username}</h5>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
